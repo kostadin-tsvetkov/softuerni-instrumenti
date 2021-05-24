e540a34119afe3c90d59e11c2439616e61c93d68
Kostadin Tsvetkov <kostadintsvetkov@gmail.com>
2021-05-24 15:44:30 +0300

    project

---
+++ src/main/java/com/example/a/projectA/Controller.java
@@ -0,0 +1,12 @@
+package com.example.a.projectA;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping(value = "/")
+    public String getMessage() {
+        return "Hello";
+    }
+}
---
+++ src/test/java/com/example/a/projectA/ProjectAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.a.projectA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
