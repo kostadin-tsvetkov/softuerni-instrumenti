d3f1cedacb90df6b0b29eb1a40fbf8d8ed059d6b
Kostadin Tsvetkov <kostadintsvetkov@gmail.com>
2021-05-26 21:08:42 +0300

    add comment in controller

--- src/main/java/com/example/a/projectA/Controller.java
+++ src/main/java/com/example/a/projectA/Controller.java
@@ -3,6 +3,7 @@ package com.example.a.projectA;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//Rest controller for message generation
 @RestController
 public class Controller {
     @GetMapping(value = "/")
